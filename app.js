7f7f64455f8bad46d9b8f3bf0b30bf3621c05a30
tushar <tushar@192.168.1.102>
2015-01-17 04:24:15 -0500

    Conditional events are working!

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.post('/signup', routes.signup)
 app.get('/signup', routes.getsignup);
+app.get('/connect' , routes.connect);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,12 @@
 
+
+var spark = require('sparknode');
+
+var core = new spark.Core({
+	accessToken: '1d4cceaa50b56cd19a8e8ee0424321139dfcd920',
+	id: '53ff6b066667574847202567'
+});
+
 /*
  * GET home page.
  */
@@ -17,10 +25,18 @@ var getsignup = function(req, res) {
 	res.render('signup' , {title : 'Signup'});
 }
 
+var connect = function(req, res) {
+	core.on('Acceleration' , function(data) {
+		console.log("Data received");
+		console.log(data);
+	});
+}
+
 var routes = {
 	index: index,
 	signup: signup,
-	getsignup: getsignup
+	getsignup: getsignup,
+	connect: connect
 
 }
 
