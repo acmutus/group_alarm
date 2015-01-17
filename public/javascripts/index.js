0c446c5213348b2298c7acc48d8744b44f95dc1d
tushar <tushar@192.168.1.102>
2015-01-17 03:00:10 -0500

    Added signup page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,24 +1,24 @@
 $(function() {
 	$("#signup").click(function(e) {
 		e.preventDefault();
-		var username = $("#username").val();
-		var password = $("#password").val();
-		var email = $("#email").val();
+		//var username = $("#username").val();
+		//var password = $("#password").val();
+		//var email = $("#email").val();
 
 		$.ajax({ 
     		url: '/signup',
-    		type: 'POST', 
-    		data: { 
+    		type: 'GET', 
+    		/*data: { 
     			"username": username,
     			"password": password,
     			"email": email
-    		}, 
-    		success: function(data){
+    		},*/ 
+    		/*success: function(data){
     			$("#groups").append("<p>My groups</p>");
     		}, 
     		error: function(xhr, textStatus, err) {
     			alert(err);
-    		}
+    		}*/
     	});
 	});
 });
--- routes/routes.js
+++ routes/routes.js
@@ -4,17 +4,23 @@
  */
 
 var index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 };
 
 var signup = function(req, res) {
 	res.send(true);
 }
 
+var getsignup = function(req, res) {
+	res.render('signup' , {title : 'Signup'});
+}
+
 var routes = {
 	index: index,
-	signup: signup
+	signup: signup,
+	getsignup: getsignup
 
 }
+
 
 module.exports = routes;
